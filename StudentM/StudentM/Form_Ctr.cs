d8fddc0e7590b971f9ffaa412621556c95e5d9e7
闫鹏举 <2528265471@qq.com>
2017-09-08 09:18:01 +0800

    新增Ctr窗口

--- StudentM/StudentM/Form_Ctr.cs
+++ StudentM/StudentM/Form_Ctr.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace StudentM
 {
-    public partial class Form1 : Form
+    public partial class Form_Ctr : Form
     {
-        public Form1()
+        public Form_Ctr()
         {
             InitializeComponent();
         }
