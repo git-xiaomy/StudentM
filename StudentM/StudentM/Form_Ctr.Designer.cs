54ccf3f1123ef5d49d0168bba742a0af2ab417e6
闫鹏举 <2528265471@qq.com>
2017-09-08 15:01:41 +0800

    查询代码

--- StudentM/StudentM/Form_Ctr.Designer.cs
+++ StudentM/StudentM/Form_Ctr.Designer.cs
@@ -28,32 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.姓名 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.班级 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.学分 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
-            // button1
+            // listView1
             // 
-            this.button1.Location = new System.Drawing.Point(91, 78);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "更改2";
-            this.button1.UseVisualStyleBackColor = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ID,
+            this.姓名,
+            this.班级,
+            this.学分});
+            this.listView1.Location = new System.Drawing.Point(12, 21);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(380, 254);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // ID
+            // 
+            this.ID.Text = "ID";
+            this.ID.Width = 83;
+            // 
+            // 姓名
+            // 
+            this.姓名.Text = "姓名";
+            this.姓名.Width = 87;
+            // 
+            // 班级
+            // 
+            this.班级.Text = "班级";
+            this.班级.Width = 112;
+            // 
+            // 学分
+            // 
+            this.学分.Text = "学分";
+            this.学分.Width = 131;
             // 
             // Form_Ctr
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(615, 300);
+            this.Controls.Add(this.listView1);
             this.Name = "Form_Ctr";
             this.Text = "Form_Ctr";
+            this.Load += new System.EventHandler(this.Form_Ctr_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader ID;
+        private System.Windows.Forms.ColumnHeader 姓名;
+        private System.Windows.Forms.ColumnHeader 班级;
+        private System.Windows.Forms.ColumnHeader 学分;
+
     }
 }
--- StudentM/StudentM/Form_Ctr.cs
+++ StudentM/StudentM/Form_Ctr.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace StudentM
 {
     public partial class Form_Ctr : Form
     {
+        String Sql;
+        SqlConnection conn;
         public Form_Ctr()
         {
             InitializeComponent();
+        }
+
+        private void Form_Ctr_Load(object sender, EventArgs e)
+        {
+            Sql = "select * from Class_three";
+            SqlCommand Scd = new SqlCommand(Sql,conn);
+            SqlDataAdapter sda = new SqlDataAdapter();//Adapter适配器
+            sda.SelectCommand = Scd;
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+            listView1.Items.Clear();
+            for (int i = 0; i < ds.Tables[0].Rows.Count;i++ )
+            {
+
+            }
         }
     }
 }
