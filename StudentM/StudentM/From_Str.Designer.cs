86a25b3bd482eaf63982429e4a588cf213534ea9
宗涛 <1936631741@qq.com>
2017-09-08 09:48:27 +0800

    删除按钮

--- StudentM/StudentM/From_Str.Designer.cs
+++ StudentM/StudentM/From_Str.Designer.cs
@@ -28,24 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(85, 61);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "宗涛";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // From_Str
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(541, 308);
             this.Name = "From_Str";
             this.Text = "From_Str";
             this.ResumeLayout(false);
@@ -54,6 +43,5 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
     }
 }
