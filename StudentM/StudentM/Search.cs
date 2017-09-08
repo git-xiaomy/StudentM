ace7a4440235a1b416653461025effb6cccb22e0
闫鹏举 <2528265471@qq.com>
2017-09-08 17:51:51 +0800

    完善查询

--- StudentM/StudentM/Search.cs
+++ StudentM/StudentM/Search.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
+using System.Windows.Forms;
 
 namespace StudentM
 {
@@ -13,19 +14,25 @@ namespace StudentM
     /// </summary>
     class Search
     {
-       static string sql = "";
-
-       static SqlConnection conn;
+       static string sql = "";       
        public static DataSet Sway()//查询的方法，返回值DataSet
         {
-           
-            sql = "select * from basic";
-            SqlCommand Com = new SqlCommand(sql, conn);//为执行sql做准备
-            SqlDataAdapter sda = new SqlDataAdapter();//Adapter适配器
-            sda.SelectCommand = Com;
+            SqlCommand Com;
+            SqlDataAdapter sda;
             DataSet ds = new DataSet();
-            sda.Fill(ds);
-            return ds;
+            if (conn.connsta == false)
+            {
+                MessageBox.Show("数据库连接异常");
+            }
+            else {
+                sql = "select * from basic";
+                Com = new SqlCommand(sql, conn.conn);//为执行sql做准备
+                sda = new SqlDataAdapter();//Adapter适配器
+                sda.SelectCommand = Com;
+                sda.Fill(ds);
+            }                
+                return ds;
+            
             
         }
     }
