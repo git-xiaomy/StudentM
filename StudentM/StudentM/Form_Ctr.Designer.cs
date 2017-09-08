d3986f720002cde25501011cc9f8084496694569
闫鹏举 <2528265471@qq.com>
2017-09-08 09:32:25 +0800

    新增CTR窗口

--- StudentM/StudentM/Form_Ctr.Designer.cs
+++ StudentM/StudentM/Form_Ctr.Designer.cs
@@ -28,32 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(91, 78);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // Form_Ctr
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button1);
-            this.Name = "Form_Ctr";
-            this.Text = "Form_Y";
-            this.ResumeLayout(false);
-
+            this.Text = "Form_Ctr";
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
